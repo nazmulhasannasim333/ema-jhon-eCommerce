9c13976f613341c94b9a60a3b233a458a00276ab
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2023-03-29 23:44:49 +0600

    solve all problem

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,9 +4,12 @@ import './Cart.css';
 const Cart = ({cart}) => {
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
+        product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
@@ -14,7 +17,7 @@ const Cart = ({cart}) => {
         <div className='cart-container'>
             <h2 >Order summery</h2>
                 <div className='cart-info'>
-                <p>Selected Items: {cart.quantity}</p>
+                <p>Selected Items: {quantity}</p>
                 <p>Total Price: ${totalPrice}</p>
                 <p>Total Shipping Charge: ${totalShipping}</p>
                 <p>Tax: ${tax.toFixed(2)}</p>
